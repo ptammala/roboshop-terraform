ba1468dd89023e8cd04a9f7975c6f5e6c126765c
github-repos <sheshankprakash260@gmail.com>
2023-08-22 00:20:54 +0530

    tfvars

---
+++ env-dev/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "tf-state-ptb74"
+key    = "terraform/tfstate"
+region = "us-east-1"
---
+++ main.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+  }
+}
+
+variable "test"{}
+
+output "test" {
+  value = var.test
+}
